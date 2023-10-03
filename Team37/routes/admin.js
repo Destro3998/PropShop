443a84e63169cc718a9802125e3c05fe7eeccbc7
Noah Martens <urf876@usask.ca>
2023-10-03 15:08:10 -0600

    Added basic item cataloge to 'Store' page.

--- Team37/routes/admin.js
+++ Team37/routes/admin.js
@@ -5,14 +5,24 @@ const path = require("path");
 router.get("/dashboard", (req, res) => {
 	let props = ["first", "second", "third", "fourth", "prop1", "prop2", "prop2", "second last", "last"]
 	let clients = ["Dylan", "Clint", "Sukhman", "Shubham", "Noah", "Theo"]
-	res.render("dashboard.handlebars", {name: "Dashboard Page", props: props, clients: clients});
+	res.render("dashboard.handlebars", { name: "Dashboard Page", props: props, clients: clients });
 });
 
-router.get("/add-prop", (req, res)=>{
+router.get("/add-prop", (req, res) => {
 	res.render("addProp.handlebars");
 })
 
-router.post("/add-prop", (req, res)=>{
+router.post("/add-prop", (req, res) => {
+	console.log(req.body);
+	res.status(200)
+	res.redirect("/admin/dashboard");
+});
+
+router.get("/add-client", (req, res) => {
+	res.render("addClient.handlebars");
+})
+
+router.post("/add-client", (req, res) => {
 	console.log(req.body);
 	res.status(200)
 	res.redirect("/admin/dashboard");
--- Team37/routes/index.js
+++ Team37/routes/index.js
@@ -3,19 +3,24 @@ const router = express.Router();
 const path = require("path")
 
 router.get("/", (req, res) => {
-	res.render("index.handlebars", {name: "Index Page"});
+	res.render("index.handlebars", { name: "Index Page" });
 });
 
 router.get("/about", (req, res) => {
-	res.render("about.handlebars", {name: "About Page"});
+	res.render("about.handlebars", { name: "About Page" });
 });
 
 router.get("/contact", (req, res) => {
-	res.render("contact.handlebars", {name: "Contact Page"});
+	res.render("contact.handlebars", { name: "Contact Page" });
 });
 
 router.get("/store", (req, res) => {
-	res.render("store.handlebars", {name: "store Page"});
+	let props = [{ name: "first", image: "image" },
+	{ name: "second", image: "image" },
+	{ name: "third", image: "image" },
+	{ name: "fourth", image: "image" },
+	{ name: "last", image: "image" }];
+	res.render("store.handlebars", { name: "Store Page", props: props });
 });
 
 module.exports = router;
