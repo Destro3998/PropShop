95616016fe1a2cb644990202bf62abd9135c7863
Theophilus Nenhanga <65165568+TeeEnnEnn@users.noreply.github.com>
2023-10-11 01:35:38 -0600

    Minor Changes - Switching Branch
    * Added QR code button
    * Added active page indicators to login and register pages.

--- Team37/routes/accounts.js
+++ Team37/routes/accounts.js
@@ -6,14 +6,14 @@ const router = express.Router();
  * This is a route for registering users
  */
 router.get("/register", (req, res) => {
-	res.render("register.handlebars", {name: "Register Page"});
+	res.render("register.handlebars", {name: "Register Page", registerActive:true});
 });
 
 /**
  * This is a route for logging users in
  */
 router.get("/login", (req, res) => {
-	res.render("login.handlebars", {name: "Login Page"});
+	res.render("login.handlebars", {name: "Login Page", loginActive:true});
 });
 
 // This allows other files to import the router
